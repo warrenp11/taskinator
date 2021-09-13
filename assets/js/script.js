53acd2cde58f86c6dd672fc745f85655a8fecd28
Peter <warrenp11@gmail.com>
2021-09-13 17:25:26 -0400

    add ability to edit tasks, not able to resave tasks yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -90,17 +90,36 @@ var createTaskActions = function(taskId) {
 };
 
 var taskButtonHandler = function(event) {
+    // get target element from event
+    var targetEl = event.target;
 
-    if (event.target.matches(".delete-btn")) {
-        // get element's task id
-        var taskId = event.target.getAttribute("data-task-id");
+    // edit button was clicked
+    if (targetEl.matches(".edit-btn")) {
+        var taskId = targetEl.getAttribute("data-task-id");
+        editTask(taskId);
     }
 
-    // call deleteTask() function
-    if (event.target.matches(".delete-btn")) {
-        var taskId = event.target.getAttribute("data-task-id");
+    // delete button was clicked
+    else if (targetEl.matches(".delete-btn")) {
+        var taskId = targetEl.getAttribute("data-task-id");
         deleteTask(taskId);
     }
+};
+
+var editTask = function(taskId) {
+    console.log("editing task #" + taskId);
+    // get task list item element
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    // get content from task name and type
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    //console.log(taskName);
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+    //console.log(taskType);
+
+    document.querySelector("input[name='task-name']").value = taskName;
+    document.querySelector("select[name='task-type']").value = taskType;
+    document.querySelector("#save-task").textContent = "Save Task";
+    formEl.setAttribute("data-task-id", taskId);
 };
 
 var deleteTask = function(taskId) {
